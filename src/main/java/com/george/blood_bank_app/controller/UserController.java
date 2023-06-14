5942f69fa1fff9ac12e590893d7e41522655067a
George Amuzu <gnamuzu@gmail.com>
2023-06-14 10:47:30 +0100

    added user authenticate method and login path.

--- src/main/java/com/george/blood_bank_app/controller/UserController.java
+++ src/main/java/com/george/blood_bank_app/controller/UserController.java
@@ -1,22 +1,28 @@
 package com.george.blood_bank_app.controller;
 
+import com.george.blood_bank_app.dao.UserDao;
+import com.george.blood_bank_app.model.User;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet({
-        "/user_login"
+        "/user_login", "/user_authenticate"
 })
 public class UserController extends HttpServlet {
 
+    private UserDao userDao = new UserDao();
+
     @Override
     public void init() {
-
+        userDao = new UserDao();
     }
 
     @Override
@@ -33,6 +39,9 @@ public class UserController extends HttpServlet {
                 case "/user_login":
                     userLogin(request, response);
                     break;
+                case "/user_authenticate":
+                    userAuthenticate(request, response);
+                    break;
                 default:
                     RequestDispatcher dispatcher = request.getRequestDispatcher("pages/user/login.jsp");
                     dispatcher.forward(request, response);
@@ -45,5 +54,28 @@ public class UserController extends HttpServlet {
     private void userLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("pages/user/login.jsp");
         dispatcher.forward(request, response);
+    }
+
+    private void userAuthenticate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            User user = userDao.validateUser(username, password);
+            String destPage = "/user_login";
+            HttpSession session = request.getSession();
+
+            if (user != null) {
+                session.setAttribute("user", user);
+                destPage = "/";
+            } else {
+                session.setAttribute("errorMsg", "Invalid username or password.");
+            }
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+            dispatcher.forward(request, response);
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
     }
 }
