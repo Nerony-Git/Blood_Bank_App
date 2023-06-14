b1b0b7056930e9e941343135d0bf045085ed8a38
George Amuzu <gnamuzu@gmail.com>
2023-06-14 10:49:30 +0100

    added admin authenticate method and login path.

--- src/main/java/com/george/blood_bank_app/controller/AdminController.java
+++ src/main/java/com/george/blood_bank_app/controller/AdminController.java
@@ -1,22 +1,29 @@
 package com.george.blood_bank_app.controller;
 
+import com.george.blood_bank_app.dao.AdminDao;
+import com.george.blood_bank_app.model.Admin;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet({
-        "/admin_login", "/contact_us", "/about_us"
+        "/admin_login", "/contact_us", "/about_us", "/admin_authenticate", "/admin_dashboard",
+        "/admin_profile"
 })
 public class AdminController extends HttpServlet {
 
+    private AdminDao adminDao = new AdminDao();
+
     @Override
     public void init(){
-
+        adminDao = new AdminDao();
     }
 
     @Override
@@ -38,6 +45,15 @@ public class AdminController extends HttpServlet {
                     break;
                 case "/about_us":
                     aboutUs(request, response);
+                    break;
+                case "/admin_authenticate":
+                    adminAuthenticate(request, response);
+                    break;
+                case "/admin_dashboard":
+                    adminDashboard(request, response);
+                    break;
+                case "/admin_profile":
+                    adminProfile(request, response);
                     break;
                 default:
                     RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/login.jsp");
@@ -61,5 +77,38 @@ public class AdminController extends HttpServlet {
     private void aboutUs(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("pages/web/about_us.jsp");
         dispatcher.forward(request, response);
+    }
+
+    private void adminDashboard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/dashboard.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void adminProfile(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/view_profile.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void adminAuthenticate(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            Admin admin = adminDao.validateAdmin(username, password);
+            String destPage = "/admin_login";
+            HttpSession session = request.getSession();
+
+            if (admin != null) {
+                session.setAttribute("admin", admin);
+                destPage = "/admin_dashboard";
+            } else {
+                session.setAttribute("errorMsg", "Invalid username or password.");
+            }
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+            dispatcher.forward(request, response);
+        } catch (SQLException e) {
+            throw new ServletException();
+        }
     }
 }
