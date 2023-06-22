d20b0ac38fc3af9142cc3e3bd1519cda64c872d9
George Amuzu <gnamuzu@gmail.com>
2023-06-22 11:22:10 +0100

    added DonationCamp model.

---
+++ src/main/java/com/george/blood_bank_app/model/DonationCamp.java
@@ -0,0 +1,73 @@
+package com.george.blood_bank_app.model;
+
+public class DonationCamp {
+    private String campID;
+    private String campName;
+    private String organizers;
+    private String address;
+    private String postal_address;
+    private String details;
+
+
+    public DonationCamp() {
+
+    }
+
+    public DonationCamp(String campID, String campName, String organizers, String address, String postalAddress, String details) {
+        super();
+        this.campID = campID;
+        this.campName = campName;
+        this.organizers = organizers;
+        this.address = address;
+        this.postal_address = postalAddress;
+        this.details = details;
+    }
+
+    public String getCampID() {
+        return campID;
+    }
+
+    public void setCampID(String campID) {
+        this.campID = campID;
+    }
+
+    public String getCampName() {
+        return campName;
+    }
+
+    public void setCampName(String campName) {
+        this.campName = campName;
+    }
+
+    public String getOrganizers() {
+        return organizers;
+    }
+
+    public void setOrganizers(String organizers) {
+        this.organizers = organizers;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostal_address() {
+        return postal_address;
+    }
+
+    public void setPostal_address(String postal_address) {
+        this.postal_address = postal_address;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
